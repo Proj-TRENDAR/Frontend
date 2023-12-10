23765287ccf433ed0a622e8c45b8bae4b007de0a
soohyeon <su132489@gmail.com>
2023-12-10 18:49:10 +0900

    Feat: 유저 모달의 프로필, 캘린더 관리에 준비중 모달 띄우기

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -5,7 +5,11 @@ import IconButton from '@components/common/button/IconButton'
 import ButtonsModal from '@components/common/modal/ButtonsModal'
 import * as S from './style'
 
-export default function Header() {
+interface Props {
+  [key: string]: any
+}
+
+export default function Header({ ...props }: Props) {
   const navigate = useNavigate()
   let userImage = null // TODO : 유저 이미지 받아서 출력해야함. 임의로 null
 
@@ -18,13 +22,14 @@ export default function Header() {
           </IconButton>
         }
       >
-        {/* TODO : 페이지 생성 시 아래 링크 추가하기 */}
-        {/* <li>
-          <Link to="/">프로필</Link>
+        <li>
+          {/* TODO: 프로필 페이지 생성시 Link로 수정하기 */}
+          <button onClick={props.handleOpenForComingSoonModal}>프로필</button>
         </li>
         <li>
-          <Link to="/">캘린더 관리</Link>
-        </li> */}
+          {/* TODO: 캘린더 관리 페이지 생성시 Link로 수정하기 */}
+          <button onClick={props.handleOpenForComingSoonModal}>캘린더 관리</button>
+        </li>
         <li>
           <button
             className="red"
