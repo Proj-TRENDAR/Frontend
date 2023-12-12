7e9109ea6154b991e27511d3bfcca16db6201a12
ohseong_kim <bkks1004@naver.com>
2023-12-12 22:57:34 +0900

    Refactor: kakao login svg 적용

--- src/components/common/Login/Kakao/index.tsx
+++ src/components/common/Login/Kakao/index.tsx
@@ -1,14 +1,15 @@
-import KakaoLogo from '@/assets/image/kakaoLogo.svg'
+import KakaoLogo from '@/assets/image/kakaoLogo.svg?react'
 
-const KakaoLogin = () => {
+export default function KakaoLogin() {
   const CLIENT_ID = `${import.meta.env.VITE_KAKAO_REST_API_KEY}`
   const REDIRECT_URI = `${import.meta.env.VITE_KAKAO_REDIRECT_URL}`
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
   const handleLogin = () => {
     window.location.href = kakaoURL
   }
-  // FIXME: img 태그를 svg or KakaoLogo를 바로 사용하는 방안 적용
-  return <img src={KakaoLogo} alt="Kakao Logo" style={{ cursor: 'pointer' }} onClick={handleLogin} />
+  return (
+    <>
+      <KakaoLogo style={{ cursor: 'pointer' }} onClick={handleLogin} />
+    </>
+  )
 }
-
-export default KakaoLogin
