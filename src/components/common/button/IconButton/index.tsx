ae81378558eca4b502a9a7e89f6b9bfe6de6fcbc
soohyeon <su132489@gmail.com>
2024-04-19 14:18:27 +0900

    fix: IconButton 컴포넌트 타입 수정

--- src/components/common/button/IconButton/index.tsx
+++ src/components/common/button/IconButton/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import * as S from './style'
 
 interface Props {
-  onClick: () => void
+  onClick?: () => void
   children: React.ReactNode
   style?: React.CSSProperties
 }
