7ab9101149c2223d4077aa64b16c55b54d0786f8
soohyeon <su132489@gmail.com>
2024-07-30 00:49:57 +0900

    feat: 우측바에 선택된 날짜의 일정 표출

--- src/pages/Home/Event/index.tsx
+++ src/pages/Home/Event/index.tsx
@@ -30,24 +30,30 @@ export default function Event({ id }: Props) {
     const { prevMonthDates, thisMonthDates, nextMonthDates } = calendar.getMonthDates(selectedDate)
     const monthlyDates = [...prevMonthDates, ...thisMonthDates, ...nextMonthDates]
 
-    let beforeDatesOfSelectedDate: IEvent[] = [] // 선택된 날짜가 포함된 주 첫날(일요일) ~ 선택된 날짜 전까지의 일정
-    let weekOfselectedDate: IEvent[] = [] // 선택된 날짜의 일정
+    let weekOfSelectedDate: IEvent[] = [] // 선택된 날짜가 포함된 주 첫날(일요일) ~ 선택된 날짜 전까지의 일정
     for (let i = 0; i < monthlyDates.length; i += 7) {
       if (monthlyDates[i] <= selectedDate && monthlyDates[i + 7] >= selectedDate) {
-        beforeDatesOfSelectedDate = calendarInfo.selectedMonthInfo.slice(i, i + selectedDate.getDay()).flat()
-        weekOfselectedDate = calendarInfo.selectedMonthInfo[i + selectedDate.getDay()] ?? []
-        break
+        const monthStartDay = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay()
+        const selectedDateOfDate = selectedDate.getDate()
+        for (let i = 0; i < calendarInfo.selectedMonthInfo.length; i++) {
+          if (i * 7 + 7 - monthStartDay >= selectedDateOfDate) {
+            weekOfSelectedDate = calendarInfo.selectedMonthInfo[i]
+            break
+          }
+        }
       }
     }
 
     // 2. 하루 일정은 아래 순서대로 출력되어야함
-    // 2-1. beforeDatesOfSelectedDate 중 하루종일, 하루이상이면서 선택된 날짜까지 이어지는 일정
+    // 2-1. weekOfselectedDate 중 하루종일, 하루이상이면서 선택된 날짜까지 이어지는 일정
     // 2-2. 선택된 날짜의 일정
+    //  FIXME: 여기서부터 잘못되어서 못가져오는것 같다!
     setEventList([
-      ...beforeDatesOfSelectedDate.filter(
-        event => event?.being !== null && new Date(event?.endTime).getTime() >= selectedDate.getTime()
+      ...weekOfSelectedDate.filter(
+        event =>
+          new Date(event?.startTime).getTime() <= selectedDate.getTime() &&
+          new Date(event?.endTime).getTime() >= selectedDate.getTime()
       ),
-      ...weekOfselectedDate,
     ])
   }, [calendarInfo])
 
