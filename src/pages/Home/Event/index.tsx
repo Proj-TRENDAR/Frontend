84a0f1dec1402c7b684bcc7cbc9a60bbdbf4ff7b
soohyeon <su132489@gmail.com>
2024-08-05 02:51:37 +0900

    comment, fix: 불필요한 주석 삭제. 타입 오류 수정

--- src/pages/Home/Event/index.tsx
+++ src/pages/Home/Event/index.tsx
@@ -25,36 +25,46 @@ export default function Event({ id }: Props) {
 
   useEffect(() => {
     // 1. 선택 된 날짜가 포함된 일정 구하기
-    const selectedDate = new Date(calendarInfo.selectedDate)
+    if (calendarInfo && calendarInfo.selectedDate) {
+      const selectedDate = new Date(calendarInfo.selectedDate)
 
-    const { prevMonthDates, thisMonthDates, nextMonthDates } = calendar.getMonthDates(selectedDate)
-    const monthlyDates = [...prevMonthDates, ...thisMonthDates, ...nextMonthDates]
+      const { prevMonthDates, thisMonthDates, nextMonthDates } = calendar.getMonthDates(selectedDate)
+      const monthlyDates = [...prevMonthDates, ...thisMonthDates, ...nextMonthDates]
 
-    let weekOfSelectedDate: IEvent[] = [] // 선택된 날짜가 포함된 주 첫날(일요일) ~ 선택된 날짜 전까지의 일정
-    for (let i = 0; i < monthlyDates.length; i += 7) {
-      if (monthlyDates[i] <= selectedDate && monthlyDates[i + 7] >= selectedDate) {
-        const monthStartDay = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay()
-        const selectedDateOfDate = selectedDate.getDate()
-        for (let i = 0; i < calendarInfo.selectedMonthInfo.length; i++) {
-          if (i * 7 + 7 - monthStartDay >= selectedDateOfDate) {
-            weekOfSelectedDate = calendarInfo.selectedMonthInfo[i]
-            break
+      let weekOfSelectedDate: IEvent[] = [] // 선택된 날짜가 포함된 주 첫날(일요일) ~ 선택된 날짜 전까지의 일정
+      for (let i = 0; i < monthlyDates.length; i += 7) {
+        if (monthlyDates[i] <= selectedDate && monthlyDates[i + 7] >= selectedDate) {
+          const monthStartDay = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay()
+          const selectedDateOfDate = selectedDate.getDate()
+          for (let i = 0; i < calendarInfo.selectedMonthInfo.length; i++) {
+            if (i * 7 + 7 - monthStartDay >= selectedDateOfDate) {
+              weekOfSelectedDate = calendarInfo.selectedMonthInfo[i]
+              break
+            }
           }
         }
       }
-    }
 
-    // 2. 하루 일정은 아래 순서대로 출력되어야함
-    // 2-1. weekOfselectedDate 중 하루종일, 하루이상이면서 선택된 날짜까지 이어지는 일정
-    // 2-2. 선택된 날짜의 일정
-    //  FIXME: 여기서부터 잘못되어서 못가져오는것 같다!
-    setEventList([
-      ...weekOfSelectedDate.filter(
-        event =>
-          new Date(event?.startTime).getTime() <= selectedDate.getTime() &&
-          new Date(event?.endTime).getTime() >= selectedDate.getTime()
-      ),
-    ])
+      // 2. 하루 일정은 아래 순서대로 출력되어야함
+      // 2-1. weekOfselectedDate 중 하루종일, 하루이상이면서 선택된 날짜까지 이어지는 일정
+      // 2-2. 선택된 날짜의 일정
+      setEventList([
+        ...weekOfSelectedDate.filter(event => {
+          // FIXME: 이런 타입 가드를 없애려면.. 이미 저장된 일정상태 타입과, 수정/저장시 일정상태 타입을 분리해야할것같은데..
+          //  이부분을 고치는게 맞는지, 아니면 이렇게 가드를 하는게 나은지는 아직 잘 모르겠어요
+          //  ex) 저장된 일정인 경우 startTime: Date
+          //  ex) 수정/저장시 일정인 경우 startTime: Date | undefined (undefined 상태 일 수 있음)
+          if (event.startTime && event.endTime) {
+            return (
+              new Date(event.startTime).getTime() <= selectedDate.getTime() &&
+              new Date(event.endTime).getTime() >= selectedDate.getTime()
+            )
+          } else {
+            return false
+          }
+        }),
+      ])
+    }
   }, [calendarInfo])
 
   return (
@@ -66,7 +76,10 @@ export default function Event({ id }: Props) {
         <PageHeader
           title={
             <h1 className="section-title">
-              {dateFormat(new Date(calendarInfo.selectedDate), 'YYYY년 MM월 DD일 d요일')}
+              {dateFormat(
+                calendarInfo.selectedDate ? new Date(calendarInfo.selectedDate) : new Date(),
+                'YYYY년 MM월 DD일 d요일'
+              )}
             </h1>
           }
           button={
