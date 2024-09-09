c36b79b7b57a11e5ddb7d33cc2b9516c4847bc8c
oskim <bkks1004@naver.com>
2024-09-09 22:21:03 +0900

    fix: routine type에 따라 분리(임시)

--- src/components/Routine/RoutineList/index.tsx
+++ src/components/Routine/RoutineList/index.tsx
@@ -10,6 +10,9 @@ import UncheckedIcon from '@assets/image/icon/check/ic-unchecked.svg?react'
 import MoreIcon from '@assets/image/icon/ic-more.svg?react'
 import { convertToKSTDate, convertToKSTDateTime } from '@/utils/dateFormat.ts'
 import { isSameDay } from 'date-fns'
+import ButtonsModal from '@components/common/modal/ButtonsModal'
+import { useTheme } from 'styled-components'
+import ButtonInAlert from '@components/common/button/ButtonInAlert'
 
 interface Props {
   list: IRoutine[]
@@ -71,16 +74,92 @@ export default function RoutineList({ list }: Props) {
   return (
     <S.RoutineList>
       {list.map((routine, index) => (
-        <S.Routine key={routine.sequence} color={routine.color}>
-          <button onClick={() => updateRoutineItem(index)}>
-            {isDone[index] ? <CheckedIcon /> : <UncheckedIcon />}
-          </button>
-          <span className={isDone[index] ? 'done' : ''}>{routine.title}</span>
-          <button className="more">
-            <MoreIcon />
-          </button>
-        </S.Routine>
+        <RoutineItem
+          key={routine.idx}
+          index={index}
+          routine={routine}
+          updateRoutineItem={updateRoutineItem}
+          isDone={isDone[index]}
+        />
       ))}
     </S.RoutineList>
+  )
+}
+
+interface RoutineItemProps {
+  index: number
+  routine: IRoutine
+  updateRoutineItem: (index: number) => Promise<void>
+  isDone: boolean
+}
+
+function RoutineItem({ index, routine, updateRoutineItem, isDone }: RoutineItemProps) {
+  const theme = useTheme()
+  const [type, setType] = useState<'basic' | 'edit' | 'stop' | 'delete'>('basic')
+
+  if (type === 'delete') {
+    return (
+      <S.Routine key={routine.sequence} color={routine.color} type={type}>
+        <span>
+          <b>'{routine.title}'</b>을(를) 삭제하시겠습니까?
+          <br />
+          삭제 시 루틴 기록도 함께 삭제됩니다.
+          <div>
+            <ButtonInAlert
+              type="cancel"
+              onClick={() => {
+                setType('basic')
+              }}
+            />
+            <ButtonInAlert
+              type="delete"
+              onClick={() => {
+                // deleteRoutineItem(routine.idx)
+              }}
+            />
+          </div>
+        </span>
+      </S.Routine>
+    )
+  }
+
+  return (
+    <S.Routine key={routine.sequence} color={routine.color} type={type}>
+      <button className="done-button" onClick={() => updateRoutineItem(index)}>
+        {isDone ? <CheckedIcon /> : <UncheckedIcon />}
+      </button>
+      <span className={isDone ? 'done' : ''}>{routine.title}</span>
+      <ButtonsModal button={<MoreIcon fill={theme.grayBtLight} />} position={{ left: '-30px' }}>
+        <li>
+          <button
+            onClick={() => {
+              console.log('수정')
+            }}
+          >
+            수정
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => {
+              console.log('중지')
+            }}
+          >
+            중지
+          </button>
+        </li>
+        <li>
+          <button
+            className="red"
+            onClick={() => {
+              console.log('삭제')
+              setType('delete')
+            }}
+          >
+            삭제
+          </button>
+        </li>
+      </ButtonsModal>
+    </S.Routine>
   )
 }
--- src/components/Routine/RoutineList/style.ts
+++ src/components/Routine/RoutineList/style.ts
@@ -1,38 +1,72 @@
 import { styled } from 'styled-components'
 
 export const RoutineList = styled.ul`
-  width: 100%;
+  //width: 100%;
   padding: 0;
   margin: 0;
 
-  display: flex;
-  flex-direction: column;
+  flex-grow: 1;
+
+  //display: flex;
+  //flex-direction: column;
   list-style: none;
 `
-export const Routine = styled.li<{ color: string }>`
+type RoutineType = 'basic' | 'edit' | 'stop' | 'delete'
+
+export const Routine = styled.li<{ color: number; type?: RoutineType }>`
   padding: 8px;
   gap: 10px;
 
   display: flex;
+  flex-direction: row;
   align-items: center;
 
-  background-color: ${props => {
-    return props?.color ? props.theme[`r${props.color}`] : props.theme.r1
-  }};
   border-radius: 8px;
+  background-color: ${({ type, color, theme }) => {
+    console.log('🚀type', type)
+    console.log('🚀color', color)
+    switch (type) {
+      case 'delete':
+        return '#4F4F4F' // theme.basicBg
+      case 'edit':
+        return theme.editBg || theme.basicBg // 기본값 설정
+      case 'stop':
+        return theme.stopBg || theme.basicBg // 기본값 설정
+      case 'basic':
+      default:
+        return color ? theme[`r${color}`] : theme.r1
+    }
+  }};
+
+  /*${({ type, color, theme }) => {
+    switch (type) {
+      case 'delete':
+        return `
+          background-color: ${theme.basicBg};
+        `
+      case 'basic':
+      default:
+        return `
+          background-color: ${color ? theme[`r${color}`] : theme.r1}};
+        `
+    }
+  }}*/
+  /*background-color: ${props => {
+    return props?.color ? props.theme[`r${props.color}`] : props.theme.r1
+  }};*/
 
   &:not(:last-child) {
     margin-bottom: 8px;
   }
 
-  button {
+  .done-button {
     margin: 0;
     padding: 0;
 
+    //display: flex;
     background: none;
     border: none;
 
-    font-size: 0;
     line-height: unset;
   }
 
